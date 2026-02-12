85e5dd113d73f28d17d5cbc41ba204211171da9c
William <133374335+WillyWilliam00@users.noreply.github.com>
2026-02-12 18:56:10 +0100

    inizializzata repo e installate librerie

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
+import AppLayout from "@/components/AppLayout";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import RegistraCopie from "./components/RegistraCopie";
+import DashboardDocenti from "./components/DashboardDocenti";
+import GestioneDocenti from "./components/GestioneDocenti";
 export function App() {
-  return (
-    <div className="flex items-center justify-center min-h-screen">
-      <div className="font-medium">Hello World</div>
-    </div>
-  )
+return (
+  <BrowserRouter>
+  <Routes>
+    <Route element={<AppLayout />}>
+      <Route path="/" element={<RegistraCopie />} />
+      <Route path="/dashboard-insegnanti" element={<DashboardDocenti />} />
+      <Route path="/gestione-docenti" element={<GestioneDocenti />} />
+    </Route>
+  </Routes>
+  </BrowserRouter>
+);
 }
 
-export default App
+export default App;
